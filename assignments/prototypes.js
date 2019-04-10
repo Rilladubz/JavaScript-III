0a8816fc2b46b630055d9f544878ecb42976abf8
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2019-04-10 18:48:43 -0400

    complete this.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,13 +1,23 @@
 /*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
+  Object oriented design is commonly used in video games.
+  For this part of the assignment you will be implementing several
+  constructor functions with their correct inheritance hierarchy.
 
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+  Note: Research Inheritance for Javascript...
 
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
-  
-  Each constructor function has unique properties and methods that are defined in their block comments below:
+  In this file you will be creating three
+  constructor functions: GameObject, CharacterStats, Humanoid.<<<< KEY WORD CONSTRUCTOR
+
+  At the bottom of this file are 3 objects that all end up
+  inheriting from Humanoid.  Use the objects at the bottom of the page
+   to test your constructor functions.
+
+   HUMANOID === CONTRUCTOR FUNCTION
+
+  Each constructor function has unique properties and methods that
+  are defined in their block comments below:<<<< HOW DO METHODS NESTED IN FUNCTIONS LOOK???
 */
-  
+
 /*
   === GameObject ===
   * createdAt
@@ -32,7 +42,7 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,68 +51,71 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
-  });
+const GameObject = 
 
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Tongue',
-  });
 
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
 
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  healthPoints: 5,
+  name: 'Bruce',
+  team: 'Mage Guild',
+  weapons: [
+    'Staff of Shamalama',
+  ],
+  language: 'Common Tongue',
+});
+
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2,
+  },
+  healthPoints: 15,
+  name: 'Sir Mustachio',
+  team: 'The Round Table',
+  weapons: [
+    'Giant Sword',
+    'Shield',
+  ],
+  language: 'Common Tongue',
+});
+
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  healthPoints: 10,
+  name: 'Lilith',
+  team: 'Forest Kingdom',
+  weapons: [
+    'Bow',
+    'Dagger',
+  ],
+  language: 'Elvish',
+});
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,103 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
+
+* 1. In implicit binding the this keyword refers to what ever is to
+the left of the dot notation in a function.
+
+* 2. Explicit Binding is utilized when a function is written in the global scope
+rather than being utilized as a method nested inside of an object. if this function
+utilizes the this keyword then anything to the right of the dot notation refers to the object
+while to the left invokes the function. Essentially gives context to what this is going to be while
+utilizing call,apply, or bind.
+
+
+
+
+* 3. In new binding this refers to a constructor function which when called utlizing the new keyword
+binds the this keyword to the context giving with in the function scope.
+
+
+
+* 4. In window when neither of the above apply this is bound to the window unless strict is applied then it
+would just kick back as undefined.
+
 */
 
-// Principle 1
 
-// code example for Window Binding
+// Principle 1 - Implicit Binding Below
+
+let animalSound = function (obj) {
+    obj.makeSound = function () {
+        console.log(this.sound);
+    };
+};
+
+let cow = {
+    sound: 'Moooo!',
+};
+
+let cat = {
+    sound: 'Meeeooow!',
+};
+
+animalSound(cow);
+animalSound(cat);
+
+cow.makeSound();
+cat.makeSound();
+
+
+
+
 
 // Principle 2
+// code example for Explicit Binding
 
-// code example for Implicit Binding
+let travelhistory = function (dest1, dest2, dest3) {
+    console.log(`${this.name} has traveled to ${dest1}, ${dest2}, ${dest3}`);
+};
+
+let Sam = {
+    name: 'Sam',
+};
+
+let destinations = ['Qatar', ' California USA', ' Florida USA'];
+
+travelhistory.apply(Sam, destinations);
 
 // Principle 3
 
 // code example for New Binding
 
+let Profile = function (username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+};
+
+let myprofile = new Profile('Rilla', 'rillatube@gmail.com', 'Abc123');
+
+console.log(myprofile);
+
+
+
+
+
 // Principle 4
 
 // code example for Explicit Binding
+// Works in browser I am assuming that vscode doesn't have a window????
+
+let animalNoise = function () {
+    console.log(this.sound);
+};
+
+let pig = {
+    sound: 'Oooooink!',
+};
+
+animalNoise();
+window.sound = 'oink';
+animalNoise();
+
+
